7932ce311362d3c8272e7c704e762d51148181cb
Alessandro Mandaglio <ale.manda.30@gmail.com>
2021-11-10 16:25:18 +0100

    aggiunti

--- Dadi/js/script-dadi.js
+++ Dadi/js/script-dadi.js
@@ -1,12 +1,13 @@
 // Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte ? Se dobbiamo confrontare qualcosa che "cosa" ci serve ?
 
-
+// genero numeri random
 let giocatore = Math.floor(Math.random() * 6) + 1;
 console.log("il dato dei giocatore " + giocatore);
 let computer = Math.floor(Math.random() * 6) + 1;
 console.log("il dato del computer " + computer);
 
+// condizione per sapere il risultato
 if (giocatore > computer) {
     console.log("Vince il giocatore!");
 } else if (computer > giocatore) {
